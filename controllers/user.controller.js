00c8e48c67295228f5aed2f6ad9e579f05e630c0
Aditya <adityabanduke@gmail.com>
2023-05-05 22:43:29 +0530

    function modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,36 +13,36 @@ export const updateUser = async (req,res)=>{
           { $set: req.body },
           { new: true }
         );
-        res.status(200).json(updateUser);
+        return res.status(200).json(updateUser);
       } catch (error) {
-        res.status(404).send("error");
+        return res.status(404).send("error");
 
       }
 }
 export const deleteUser = async (req,res)=>{
     try {
         await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User deleted sucsessully");
+        return res.status(200).json("User deleted sucsessully");
       }  catch (error) {
-        res.status(404).send("error");
+        return res.status(404).send("error");
 
       }
 }
 export const getUser = async (req,res)=>{
     try {
         const user = await User.findById(req.params.id);
-        res.status(200).json(user);
+        return res.status(200).json(user);
       } catch (error) {
-        res.status(404).send("error hai");
+        return res.status(404).send("error hai");
 
     }
 }
 export const getUsers = async (req,res)=>{
   try {
     const users = await User.find();
-    res.status(200).json(users);
+    return res.status(200).json(users);
   } catch (error) {
-      res.status(404).send("error");
+    return res.status(404).send("error");
     }
 }
 export const createDriver = async (req, res) => {
@@ -70,7 +70,7 @@ export const createDriver = async (req, res) => {
       const newDriverDetail = new User(data);
 
       const newDriver = await newDriverDetail.save();
-      res.status(200).json(newDriver);
+      return res.status(200).json(newDriver);
   } catch (error) {
       return serverErrorResponse(res, error.message);
   }
