0cf83ac5412e2774638a39200b62a9e8adb14214
Aditya <adityabanduke@gmail.com>
2023-05-07 02:28:54 +0530

    creditIncremented

--- controllers/driverApp.controller.js
+++ controllers/driverApp.controller.js
@@ -1,4 +1,5 @@
 import trashRequest from "../models/trashRequest.js";
+import User from "../models/User.js"
 export const acceptedRequest = async (req, res) => {
     try {
         console.log(req.params.id);
@@ -6,11 +7,21 @@ export const acceptedRequest = async (req, res) => {
             status : "accepted",
         });
         console.log(update);
+        const trashRequestUserId = update.userId;
+        console.log(trashRequestUserId);
+        const updateUserAndCredit = await User.findOneAndUpdate({ _id: trashRequestUserId }, {
+            $inc: {
+                credits: 10
+            }
+        })
+        console.log(updateUserAndCredit)
         return res.status(200).json(update);
     } catch (error) {
         return res.send(error);
     }
 
 };
+
+
 
 
