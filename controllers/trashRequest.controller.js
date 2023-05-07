f9ff3fafcaa4ebda6b717a351654ccc57398c71f
Aryaman Swami <swamiaryaman21.as@gmail.com>
2023-05-07 15:54:50 +0530

    distance calculated

--- controllers/trashRequest.controller.js
+++ controllers/trashRequest.controller.js
@@ -1,5 +1,6 @@
 import trashRequest from "../models/trashRequest.js";
 import { serverErrorResponse, successResponse } from "../utils/response.js";
+import axios from "axios";
 
 export const newtrashRequest = async (req, res) => {
   try {
@@ -51,28 +52,84 @@ export const getAllTrashRequest = async (req, res) => {
 
 export const calculateDistance = async (req, res) => {
   try {
-
-   const destination =[];
-    const dustbins = await trashRequest.find({status : "pending"});
-
-
-
-
-
-    const callParameters = {
-      destinations: destination,
-      origins: [
-        {
+    let destination = [];
+    let tempDest = [];
+    const dustbins = await trashRequest.find({ status: "pending" ,requestType:"public"});
+    if (dustbins.length === 0) {
+      return res.status(400).json({ message: "No pending request found" });
+    }
+    if (dustbins.length > 0) {
+      dustbins.map((dustbin) => {
+        destination.push({
           point: {
-            "latitude": req.body.latitude,
-            "longitude": req.body.longitude,
+            latitude: parseFloat(dustbin.latitude),
+            longitude: parseFloat(dustbin.longitude),
           },
+        });
+
+        tempDest.push({
+          _id: dustbin._id,
+          latitude: dustbin.latitude,
+          longitude: dustbin.longitude,
+          dustbinId: dustbin.dustbinId,
+          description: dustbin.description,
+          address: dustbin.address,
+          createdAt: dustbin.createdAt,
+          userId: dustbin.userId,
+        });
+
+
+      });
+    }
+    console.log(destination);
+    let origin = [];
+    // for (let i = 0; i < destination.length; i++) {
+      
+    // }
+    origin.push({
+        point: {
+          latitude: parseFloat(req.body.latitude),
+          longitude: parseFloat(req.body.longitude),
         },
-      ],
+      });
+
+    let callParameters = {
+      origins: origin,
+      destinations: destination,
     };
 
-    const distance  = await axios.post("https://{baseURL}/routing/matrix/{versionNumber}?key=q2yukmABGuRvQD9NhkGAABCOYtIMoHFD", callParameters);
+    // https://api.tomtom.com/search/2/poiSearch/309/2C,%20Saket%20Nagar%20Road,%20Neha%20Nursery%20School,%20Habib%20Ganj%20Saket%20Nagar%20Bhopal,%20Saket%20Nagar%20Sector%202C,%20Bhopal%20462024,%20Madhya%20Pradesh%2023.213581%2077.458349.json?limit=5&lat=23.2423424&lon=77.4242304&radius=10000&key=RzroMgvAOXlpkqRJbX6AUdNu5UX7DMqb
+    let calcDistance = {};
+    const distance = await axios
+      .post(
+        "https://api.tomtom.com/routing/matrix/2?key=RzroMgvAOXlpkqRJbX6AUdNu5UX7DMqb",
+        callParameters
+      )
+      .then((res) => {
+        console.log(res.data);
+        calcDistance = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ message: "Error in calculating distance" });
+      });
 
+    console.log(distance);
+
+    // sort calcDistance on the basis of lengthInMeters
+   calcDistance.data.map((loc,index)=>{
+    tempDest[index].distance = loc.routeSummary.lengthInMeters;
+    tempDest[index].drivingTime = loc.routeSummary.travelTimeInSeconds;
+   })
+
+    tempDest.sort((a,b)=>{
+      return a.distance - b.distance;
+    })
+
+
+    return res.status(200).json(tempDest);
     // return new Promise((resolve, reject) => {
     //   ttapi.services.matrixRouting(callParameters).then((matrixApIResults) => {
     //     const results = matrixApIResults.matrix[0];
@@ -86,7 +143,6 @@ export const calculateDistance = async (req, res) => {
     //         drivingdistance: result.response.routeSummary.lengthInMeters,
     //       };
     //     });
-        
 
     //     this.setState({ dandt: resultsArray });
     //     console.log(this.state.dandt);
