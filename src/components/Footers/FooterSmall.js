29c9f2bae3a41a2beedb16b4951a0ccb67721012
Ekansh-Patidar <84856232+Ekansh-Patidar@users.noreply.github.com>
2023-05-09 00:12:46 +0530

    footer removed

--- src/components/Footers/FooterSmall.js
+++ src/components/Footers/FooterSmall.js
@@ -10,7 +10,7 @@ export default function FooterSmall(props) {
             : "relative") + " pb-6"
         }
       >
-        <div className="container mx-auto px-4">
+        {/* <div className="container mx-auto px-4">
           <hr className="mb-6 border-b-1 border-blueGray-600" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
@@ -60,8 +60,8 @@ export default function FooterSmall(props) {
                 </li>
               </ul>
             </div>
-          </div>
-        </div>
+          </div> 
+        </div>*/}
       </footer>
     </>
   );
